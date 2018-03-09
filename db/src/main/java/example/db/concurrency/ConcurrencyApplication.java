b104a03ad219c13bd545be26bf7a797bebd6638f
gavin <gavin_feng@wtradefintech.com>
2018-03-09 10:38:08 +0800

    [REF] 添加对并发示例的注释说明

--- db/src/main/java/example/db/concurrency/ConcurrencyApplication.java
+++ db/src/main/java/example/db/concurrency/ConcurrencyApplication.java
@@ -7,6 +7,28 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
+/**
+ * 问题：
+ *     1、脏读、不可重复读、幻读、第一类更新丢失、第二类更新丢失 等
+ *     2、车辆列表页，两个人同时修改； 不是加 @Transactional 就能解决的
+ *
+ * 演示顺序：
+ *     1、默认使用 UserRepositoryJDBC，对每一种并发问题进行展示；
+ *         1.1 对于幻读，再用update方法来演示出问题；
+ *     2、更改事务的隔离级别，看是否都解决了；
+ *     3、更改为 UserRepositoryJPA，即 使用Hibernate的缓存机制；
+ *     4、将User申明为 @OptimisticLocking 进行演示
+ *
+ * 参考资料：
+ *     1、官网对锁的描述；
+ *     2、MySQL的MVCC；
+ *     3、好文章： https://segmentfault.com/a/1190000012650596
+ *     4、淘宝的数据库内核月报：  http://mysql.taobao.org/monthly/
+ *     等等
+ *
+ * 结论：
+ *     采用乐观锁的机制来防范第二类更新丢失，MySQL默认采用 REPEATABLE_READ，只有部分幻读的问题存在
+ */
 @SpringBootApplication
 @Slf4j
 public class ConcurrencyApplication implements ApplicationContextAware {
