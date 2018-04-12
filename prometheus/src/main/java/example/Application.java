f89e52959c697b197cbbd6ccbb4108bae9076bb4
gavin <gavin_feng@wtradefintech.com>
2018-04-12 13:35:49 +0800

    [IMP] micrometer-jvm-extras的引入

--- prometheus/src/main/java/example/Application.java
+++ prometheus/src/main/java/example/Application.java
@@ -1,12 +1,26 @@
 package example;
 
+import io.github.mweirauch.micrometer.jvm.extras.ProcessMemoryMetrics;
+import io.github.mweirauch.micrometer.jvm.extras.ProcessThreadMetrics;
+import io.micrometer.core.instrument.binder.MeterBinder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public MeterBinder processMemoryMetrics() {
+        return new ProcessMemoryMetrics();
+    }
+
+    @Bean
+    public MeterBinder processThreadMetrics() {
+        return new ProcessThreadMetrics();
     }
 }
